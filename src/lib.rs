05655fbacb4d3a8d95f3252a3001925722c5c920
clshu <clshu2020@gmail.com>
2022-08-16 18:22:46 +0800

    Create WebGL set up function in gl_setup.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate wasm_bindgen;
+mod gl_setup;
 
 use wasm_bindgen::prelude::*;
+use web_sys::WebGlRenderingContext as GL;
 
 #[wasm_bindgen]
 extern "C" {
@@ -14,19 +16,25 @@ pub fn say_hello_from_rust() {
 }
 
 #[wasm_bindgen]
-pub struct DougClient {}
+pub struct DougClient {
+    gl: GL,
+}
 
 #[wasm_bindgen]
 impl DougClient {
     #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
-        log("DougClient created");
-        Self {}
+        console_error_panic_hook::set_once();
+        let gl = gl_setup::initialize_webgl_context().unwrap();
+
+        Self { gl: gl }
     }
 
     pub fn update(&self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
         Ok(())
     }
 
-    pub fn render(&self) {}
+    pub fn render(&self) {
+        self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+    }
 }
