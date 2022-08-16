f01a6de81350ead2df63117f978ab1ca344f4b98
clshu <clshu2020@gmail.com>
2022-08-16 16:57:18 +0800

    Create a struct(class) with a constructor, update() and render().

--- src/lib.rs
+++ src/lib.rs
@@ -12,3 +12,24 @@ extern "C" {
 pub fn say_hello_from_rust() {
     log("Hello from Rust!");
 }
+
+#[wasm_bindgen]
+pub struct DougClient {}
+
+#[wasm_bindgen]
+impl DougClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        log("DougClient::new()");
+        Self {}
+    }
+
+    pub fn update(&self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+        log("DougClient.update()");
+        Ok(())
+    }
+
+    pub fn render(&self) {
+        log("DougClient.render()");
+    }
+}
